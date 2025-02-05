dc85507b39cd836b6eeea7f5caa695129df42db0
Firat <firatdmf@gmail.com>
2025-02-05 23:51:32 +0300

    stock at showing profile info on chat

--- django-starter-main/a_rtchat/views.py
+++ django-starter-main/a_rtchat/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required 
+from .models import *
 
 # Create your views here.
 
-
+@login_required
 def chat_view(request):
-    return render(request, 'a_rtchat/chat.html')
+    # Get all messages belong to this group
+    chat_group = get_object_or_404(ChatGroup, group_name='public-chat')
+    # Get the last 30 messages
+    chat_messages = chat_group.chat_messages.all()[:30]
+    return render(request, 'a_rtchat/chat.html', {'chat_messages': chat_messages})
