872a2055046d34c2a6b2297dcc3fbdf2c1261b00
Muhammed <firatdmf@gmail.com>
2025-02-07 14:03:58 +0300

    last save before going into redis

--- django-starter-main/a_rtchat/models.py
+++ django-starter-main/a_rtchat/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128, unique= True)
     # Fetch all the groups that this user is online in.
-    # users_online = models.ManyToManyField(User, related_name='online_in_groups', blank=True)
+    users_online = models.ManyToManyField(User, related_name='online_in_groups', blank=True)
 
     def __str__(self):
         return self.group_name
