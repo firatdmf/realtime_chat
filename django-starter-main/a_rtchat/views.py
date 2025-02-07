2b5175e924af4e3dd4bd288b8cf91d90c0790341
Muhammed <firatdmf@gmail.com>
2025-02-07 11:55:59 +0300

    changes from mac

--- django-starter-main/a_rtchat/views.py
+++ django-starter-main/a_rtchat/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required 
 from .models import *
-
+from .forms import ChatmessageCreateForm
 # Create your views here.
 
 @login_required
@@ -10,4 +10,19 @@ def chat_view(request):
     chat_group = get_object_or_404(ChatGroup, group_name='public-chat')
     # Get the last 30 messages
     chat_messages = chat_group.chat_messages.all()[:30]
-    return render(request, 'a_rtchat/chat.html', {'chat_messages': chat_messages})
+    form = ChatmessageCreateForm()
+    # if request.method == 'POST':
+    if request.htmx:
+        form = ChatmessageCreateForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            context = {'message': message, 'user': request.user}
+            return render(request, 'a_rtchat/partials/chat_message_p.html', context )
+
+
+    return render(request, 'a_rtchat/chat.html', {'chat_messages': chat_messages, 'form': form})
+
+
